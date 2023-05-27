41925a909b0db512dba1b24090bec78ec945d259
kemmily Souza <kemmilyps1@gmail.com>
2023-05-27 17:46:43 -0300

    Foi realizado melhorias para organização do codigo da class Menu

---
+++ src/main/java/Agenda.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Agenda {
+
+    public void open() {
+       showMenu();
+       selectOption();
+    }
+
+    private void showMenu() {
+        String menuOptions = String.join("\n",
+                "O que você deseja fazer?",
+                "1. Visualizar agenda",
+                "2. Visualizar favoritos",
+                "3. Criar novo contato",
+                "4. Editor contato",
+                "5. Excluir contato",
+                "6. Excluir agenda",
+                "0. Sair da Agenda"
+        );
+        System.out.println(menuOptions);
+    }
+
+    private void selectOption() {
+        System.out.print(">> ");
+        execMenuOption(new Scanner(System.in).nextInt());
+    }
+
+    private void execMenuOption(int choice) {
+        switch (choice) {
+            case 0:
+                System.out.println("Até mais!");
+                return;
+            case 1:
+                ServiceContacts.listContacts();
+                break;
+            case 2:
+                // falta implementar
+                break;
+            case 3:
+                ServiceContacts.insertContact();
+                break;
+            case 4:
+                // falta implementar
+                break;
+            case 5:
+                // falta implementar
+                break;
+            case 6:
+                // falta implementar
+                break;
+            default:
+                invalidOptions();
+        }
+
+        open();
+    }
+
+    private void invalidOptions() {
+        Util.clearScreen();
+        System.out.println("Não consegui entender. Por favor, escolha uma opção válida.");
+    }
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,3 @@
-import java.sql.*;
-import java.util.Scanner;
-
 public class Main {
 
     static String jdbcUrl = "jdbc:postgresql://localhost:5432/kemmyps";
@@ -8,11 +5,9 @@ public class Main {
     static String password = "";
 
     public static void main(String[] args) {
-//        insertContact();
-//        listContacts();
-
-        MainOptions mainOptions = new MainOptions();
-        mainOptions.mainOptions();
+        //exibi menu de opcoes
+        Agenda agenda = new Agenda();
+        agenda.open();
 
     }
 }
