0ba79594fcdc8df3b99f49f77a16ee4eea1ea481
Kemmily Souza <kemmilyps1@gmail.com>
2023-05-28 13:21:18 -0300

    Add operações de listar, adicionar e excluir contatos. (#9)
    * Add operações de listar, adicionar e excluir contatos.
    
     Também foi criada a opção de adicionar favoritos, além de listar panas contatos favoritos
    
    * Adicionado um contador para contatos
    
    Foi implementado um contador para obter a informação de quantos contatos possuem na agenda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,8 @@
 public class Main {
 
-    static String jdbcUrl = "jdbc:postgresql://localhost:5432/kemmyps";
-    static String username = "kemmyps";
-    static String password = "";
-
     public static void main(String[] args) {
         //exibi menu de opcoes
         Agenda agenda = new Agenda();
         agenda.open();
-
     }
 }
